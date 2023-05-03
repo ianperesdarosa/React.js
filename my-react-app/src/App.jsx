30fd8862b4e4306feef1be03b09d1d1569156e0a
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-05-03 03:28:59 -0300

    :fire:

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <a href="#">Lorem</a>
+          <a href="#">Lorem</a>
+          <a href="#">Lorem</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
